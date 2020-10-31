f9fd44baa76e9e50f9e9eab533cf4ae31a43c1b2
Admin <huythai2001cs@gmail.com>
2020-10-31 22:03:12 +0700

    a

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -1,26 +1,36 @@
 package uet.oop.bomberman;
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.ScaleTransition;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
-import uet.oop.bomberman.entities.Bomber;
-import uet.oop.bomberman.entities.Entity;
-import uet.oop.bomberman.entities.Grass;
-import uet.oop.bomberman.entities.Wall;
+import uet.oop.bomberman.entities.*;
 import uet.oop.bomberman.graphics.Sprite;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class BombermanGame extends Application {
     
-    public static final int WIDTH = 20;
-    public static final int HEIGHT = 15;
-    
+    public static final int WIDTH = 13;
+    public static final int HEIGHT = 31;
+    public static List<String> s;
+
+    static {
+        try {
+            s = insert();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     private GraphicsContext gc;
     private Canvas canvas;
     private List<Entity> entities = new ArrayList<>();
@@ -70,20 +80,29 @@ public class BombermanGame extends Application {
 
         createMap();
 
-        Entity bomberman = new Bomber(19, 14, Sprite.player_right.getFxImage());
+        Entity bomberman = new Bomber(1, 1, Sprite.player_right.getFxImage());
         entities.add(bomberman);
     }
 
     public void createMap() {
-        for (int i = 0; i < WIDTH; i++) {
-            for (int j = 0; j < HEIGHT; j++) {
+        for (int i = 0; i < s.size(); i++) {
+            for (int j = 0; j < s.get(i).length(); j++) {
                 Entity object;
-                if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
-                    object = new Wall(i, j, Sprite.wall.getFxImage());
-                }
-                else {
-                    object = new Grass(i, j, Sprite.grass.getFxImage());
-                }
+//                if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
+//                    object = new Wall(i, j, Sprite.wall.getFxImage());
+//                }
+//                else {
+//                    object = new Grass(i, j, Sprite.grass.getFxImage());
+//                }
+                  if (s.get(i).charAt(j) == '#') {
+                      object = new Wall(i, j, Sprite.wall.getFxImage());
+                  }
+                  else if (s.get(i).charAt(j) == '*') {
+                      object = new Brick(i, j, Sprite.brick.getFxImage());
+                  }
+                  else {
+                      object = new Grass(i, j, Sprite.grass.getFxImage());
+                  }
                 stillObjects.add(object);
             }
         }
@@ -97,5 +116,15 @@ public class BombermanGame extends Application {
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
         stillObjects.forEach(g -> g.render(gc));
         entities.forEach(g -> g.render(gc));
+    }
+
+    public static List<String> insert() throws FileNotFoundException {
+        List<String> s = new ArrayList<>();
+        Scanner scanner = new Scanner(new File("E:\\New folder (4)\\Bomberman\\res\\levels\\level.txt"));
+        while (scanner.hasNext()) {
+            String a = scanner.nextLine();
+            s.add(a);
+        }
+        return s;
     }
 }
